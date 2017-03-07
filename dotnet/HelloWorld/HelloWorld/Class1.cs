73a3b765a0bf746eb3d2dd8e46061f0b1c37dab2
nitin garg <nitin.virgo@gmail.com>
2017-03-06 23:31:18 -0700

    commit

---
+++ dotnet/HelloWorld/HelloWorld/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Class1
+    {
+
+        public static String CreateMessage() {
+            return "hello";
+        }
+    }
+
+
+}
